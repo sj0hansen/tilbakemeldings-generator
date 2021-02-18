5779d94628744394df38f37af1d4998e0138413d
sl3ipnr <64078406+sl3ipnr@users.noreply.github.com>
2021-02-18 14:09:07 +0100

    Finjusterer koden med kommentarer

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+/*
+* Javascript for Tilbakemeldingsgenerator.
+* Scriptet velger ut tilbakemeldingen som skal gis, og viser den som innerHTML.
+*
+* Sondre Johansen
+* sondre.johans1@outlook.com
+*
+* HJI:
+* Daniel Steffensen
+* Even Tomter
+*/
+
+//Array bestående av strenger med adferder
 var adferd = Array(
   "Når du velger å gjøre oppgavene dine på en innefektiv måte.",
   "Oppgavene du gjør blir til tider gjort på en mindre god måte.",
@@ -10,6 +23,7 @@ var adferd = Array(
   "I akutte situasjoner kan du virke usikker og fryse til.", "I relativt rolige situasjoner hvor en har god tid oppleves du til tider som stresset."
 );
 
+//Array bestående av strenger med reaksjoner
 var reaksjon = Array(
   "Dette gjør at jeg kan føle meg noe ubrukelig", 
   "Dette gjør at jeg blir ekstremt irritert", 
@@ -17,20 +31,27 @@ var reaksjon = Array(
   "Når dette skjer føler jeg at du ikke egentlig bryr deg om hva som skjer"
 );
 
+//Array bestående av strenger med konsekvenser
 var konsekvens = Array(
-
+  "som igjen fører til at laget i seg selv ofte jobber mindre effektivt."
 
 );
 
+
+
+//Funksjon som velger ut en streng fra hvert array og viser generert tilbakemelding over html knappen.
 function visTekst(){
 
+  //Her velges strenger fra arrayene
   let rAdferd = adferd[Math.floor(Math.random() * adferd.length)];
 
   let rReaksjon = reaksjon[Math.floor(Math.random() * reaksjon.length)];
 
-  let rKonsekvens = konsekvens[Math.floor(Math.random() * reaksjon.length)];
+  let rKonsekvens = konsekvens[Math.floor(Math.random() * konsekvens.length)];
 
-  document.getElementById("knapp").innerHTML=(rAdferd + " " + rReaksjon);
+
+  //Her settes tilbakemeldingen sammen, og vises på skjermen.
+  document.getElementById("knapp").innerHTML=(rAdferd + " " + rReaksjon + " " + rKonsekvens);
 
 }
 
